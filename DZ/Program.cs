427ef98275e570eca8c75c450c68d9aab0e31009
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-11-30 01:06:27 +0300

    решение задачи

---
+++ DZ/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+string[] array = new string[4] { "hello", "2", "world", ";-)" };
+string[] shortArray = GetShortArray(array);
+
+PrintArray(array);
+
+Console.Write("  ->  ");
+PrintArray(shortArray);
+
+/// <Получение отфильтрованного массива />
+string[] GetShortArray(string[] array)
+{
+    int count = array.Length;
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+
+    }
+    return newArray;
+}
+
+/// <Печать результата />
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] != null) Console.Write($"{array[j]} ");
+    }
+    Console.Write("]");
+}
+
+
+
+
